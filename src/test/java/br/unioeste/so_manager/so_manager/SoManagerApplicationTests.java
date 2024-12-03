4e53667c567d902f24804fa354ab4af67cbb13c8
Nikoly Cover <nikoly.pereira@pti.org.br>
2024-12-02 22:05:38 -0300

    first commit

---
+++ src/test/java/br/unioeste/so_manager/so_manager/SoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unioeste.so_manager.so_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
